e634f4920a16fd3b956d9ef70fa7553d5bf787ab
Dominykas Stasiulionis <domstasiulionis@gmail.com>
2022-10-25 13:49:20 +0100

    landing page fully responsive

--- app.js
+++ app.js
@@ -13,3 +13,35 @@ const showInter = (el) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Nav menu links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("ul a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    } else {
+      li.classList.remove("active");
+    }
+  });
+});
